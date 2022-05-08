01a749ddf4c5eccd067ba381494806eef59cd58d
Jack Cochran <64762170+jackscochran@users.noreply.github.com>
2022-05-07 23:28:14 -0400

    updated start ticker

--- data_process_manager/database/public_models/financials/adaptor.py
+++ data_process_manager/database/public_models/financials/adaptor.py
@@ -8,7 +8,7 @@ import mongoengine
 def load_data():
     tickers = company.get_all_tickers()
     begin_loading = False
-    start_ticker = 'mpa'
+    start_ticker = 'stqn'
     for ticker in tickers:
         if ticker == start_ticker:
             begin_loading = True
